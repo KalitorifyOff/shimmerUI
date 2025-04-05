1a4bee64ccbd554478f7505c569ba7b275202412
renga <abi129.off@gmail.com>
2025-04-05 19:47:32 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/shimmerpoc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shimmerpoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/shimmer_ex.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShimmerLoading extends StatelessWidget {
+  const ShimmerLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
